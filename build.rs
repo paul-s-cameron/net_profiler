52e363807e5154d776e58c8528610eb1577b4d82
Paul Cameron <116021619+paul-s-cameron@users.noreply.github.com>
2025-05-23 00:28:29 -0400

    only windows

--- build.rs
+++ build.rs
@@ -1,22 +1,24 @@
-#[cfg(target_os = "windows")]
-use winres;
 
 fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_manifest(r#"
-    <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-    <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-        <security>
-            <requestedPrivileges>
-                <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
-            </requestedPrivileges>
-        </security>
-    </trustInfo>
-    </assembly>
-    "#);
-
-    match res.compile() {
-        Ok(_) => println!("cargo:rerun-if-changed=build.rs"),
-        Err(e) => eprintln!("Error: {}", e),
+    #[cfg(target_os = "windows")]
+    {
+        use winres;
+        let mut res = winres::WindowsResource::new();
+        res.set_manifest(r#"
+        <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+        <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+            <security>
+                <requestedPrivileges>
+                    <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+                </requestedPrivileges>
+            </security>
+        </trustInfo>
+        </assembly>
+        "#);
+    
+        match res.compile() {
+            Ok(_) => println!("cargo:rerun-if-changed=build.rs"),
+            Err(e) => eprintln!("Error: {}", e),
+        }
     }
 }
